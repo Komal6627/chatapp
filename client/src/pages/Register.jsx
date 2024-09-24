0a27b4c23b3313e21b2924b2ad4655985ce9e595
Komal6627 <komalkilledar1996@gmail.com>
2024-09-24 15:07:13 +0530

    Add the CSS in Registration page

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -18,10 +18,61 @@ const FormContainer = styled.div`
     justify-content:center;
     gap: 1 rem;
     align-items: center;
+
+    img{
+    height : 4rem;
+    border-radius: 20%
   }
 
-  img{
-    height : 5rem
+  h1{
+  color: white;
+  text-trasform: uppercase;
+  }
+  }
+
+  form{
+    display: flex;
+    flex-direction: column;
+    gap: 2rem;
+    background-color : #00000076;
+    border-radius: 2rem;
+   padding: 3rem 5rem;
+    input{
+      background-color: transparent;
+      padding: 1rem;
+      border: 0.1rem solid #4e0eff;
+      border-radius: 0.4rem;
+      color: white;
+      width: 100%;
+      font-size: 1rem;
+      &:focus{
+        border : 0.1rem solid #997af0;
+      }
+    }
+
+    button{
+      background-color: #997af0;
+      color: white;
+      padding: 1rem 2rem;
+      font-weight: bold;
+      cursor: pointer;
+      border-radius: 0.4rem;
+      font-size: 1rem;
+      text-transform: uppercase;
+      transition: 0.5s ease-in-out;
+      &:hover {
+        background-color: #4e0eff
+      }
+    }
+      span {
+        color: white;
+        text-transform: uppercase;
+        a {
+          color:#4e0eff ;
+          text-decoration: none;
+          font-weight: bold
+        }
+      }
   }
 `;
 
