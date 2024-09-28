b9c1d117b25de3f58085fbaae174e10da9f78033
Komal6627 <komalkilledar1996@gmail.com>
2024-09-28 23:42:12 +0530

    working on user controller

--- server/controller/userController.js
+++ server/controller/userController.js
@@ -1,4 +1,11 @@
-export const register = (req, res, next) => {
+import bcrypt from "bcrypt"
+import User from "../model/userModel";
+
+export const register = async(req, res, next) => {
     const { username, email, password } =req.body;
-    
+    const usernameCheck = await User.findOne({username});
+
+    if(usernameCheck) {
+        return res.json({msg: "Username  already used", status: false})
+    }
 }
