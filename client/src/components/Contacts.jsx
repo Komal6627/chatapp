c3ec3ff9b2c52b3e9c285510ea3a1f90ae28212d
Komal6627 <komalkilledar1996@gmail.com>
2024-10-04 23:09:29 +0530

    Working on adding css on contact component

--- client/src/components/Contacts.jsx
+++ client/src/components/Contacts.jsx
@@ -1,40 +1,95 @@
-import React, { useEffect, useState } from 'react'
-import logo from '../assets/logo.webp'
-import styled from 'styled-components';
+import React, { useEffect, useState } from "react";
+import logo from "../assets/logo.webp";
+import styled from "styled-components";
 
-const Contacts = ({contacts, currentUser}) => {
-  const [currentUserName, setCurrentUserName ] = useState(undefined);
-  const [currentUserImage, setCurrentUserImage ] = useState(undefined);
+const Contacts = ({ contacts, currentUser }) => {
+  const [currentUserName, setCurrentUserName] = useState(undefined);
+  const [currentUserImage, setCurrentUserImage] = useState(undefined);
   const [currentSelected, setCurrentSelected] = useState(undefined);
   useEffect(() => {
-      if (currentUser) {
-        setCurrentUserImage(currentUser.avatarImage)
-        setCurrentUserName(currentUser.username);
-      }
-  },[currentUser])
+    if (currentUser) {
+      setCurrentUserImage(currentUser.avatarImage);
+      setCurrentUserName(currentUser.username);
+    }
+  }, [currentUser]);
 
-  const changeCurrentChat = (index, contact) =>{
-
-  }
+  const changeCurrentChat = (index, contact) => {};
 
   return (
     <>
-      {
-        currentUserImage && currentUserName && (
-          <Container>
-            <div className="brand">
-              <img src={logo} alt=""  />
-              <h3>Saffie</h3>
+      {currentUserImage && currentUserName && (
+        <Container>
+          <div className="brand">
+            <img src={logo} alt="logo" />
+            <h3>Saffie</h3>
+          </div>
+          <div className="contacts">
+            {contacts?.map((contact, index) => {
+              return (
+                <div
+                  className={`contact${
+                    index === currentSelected ? "selected" : ""
+                  }`}
+                  key={index}
+                >
+                  <div className="avatar">
+                    <img
+                      src={`data:image/svg+xml; base64, ${contact.avatarImage}`}
+                      alt="avatar"
+                    />
+                  </div>
+                  <div className="username">
+                    <h3>{contact.username}</h3>
+                  </div>
+                </div>
+              );
+            })}
+          </div>
+          <div className="current-user">
+            <div className="avatar">
+              <img
+                src={`data:image/svg+xml; base64, ${currentUserImage}`}
+                alt="avatar"
+              />
             </div>
-            <div className="contacts"></div>
-          </Container>
-        )
-      }
+            <div className="username">
+              <h3>{currentUserName}</h3>
+            </div>
+          </div>
+        </Container>
+      )}
     </>
-  )
-}
+  );
+};
 
+const Container = styled.div` 
+  display: grid;
+  grid-template-columns: 10% 75% 15%;
+  overflow: hidden;
+  background-color: #080420;
+  .brand{
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    gap: 1rem;
+    img{
+      height: 2rem;
+    }
+    h3{
+      color: white;
+      text-transform: uppercase;
+    }
+  }
+  .contacts{
+    display:flex;
+    flex-direction: column;
+    align-item: center;
+    overflow: auto;
+    gap: 0.8rem;
+    .contact{
+       background-color: #ffffff34;
+    }
+  }
+`;
 
-const Container = styled.div``
-
-export default Contacts
+export default Contacts;
