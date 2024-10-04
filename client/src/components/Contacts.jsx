9b4f001ecf23318ba52a93cc1a0b5c887c45e8b8
Komal6627 <komalkilledar1996@gmail.com>
2024-10-04 18:55:09 +0530

    Create contact component

--- client/src/components/Contacts.jsx
+++ client/src/components/Contacts.jsx
@@ -1,11 +1,40 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import logo from '../assets/logo.webp'
+import styled from 'styled-components';
 
-const Contacts = ({Contacts}) => {
+const Contacts = ({contacts, currentUser}) => {
+  const [currentUserName, setCurrentUserName ] = useState(undefined);
+  const [currentUserImage, setCurrentUserImage ] = useState(undefined);
+  const [currentSelected, setCurrentSelected] = useState(undefined);
+  useEffect(() => {
+      if (currentUser) {
+        setCurrentUserImage(currentUser.avatarImage)
+        setCurrentUserName(currentUser.username);
+      }
+  },[currentUser])
+
+  const changeCurrentChat = (index, contact) =>{
+
+  }
+
   return (
     <>
-        Contacts
+      {
+        currentUserImage && currentUserName && (
+          <Container>
+            <div className="brand">
+              <img src={logo} alt=""  />
+              <h3>Saffie</h3>
+            </div>
+            <div className="contacts"></div>
+          </Container>
+        )
+      }
     </>
   )
 }
+
+
+const Container = styled.div``
 
 export default Contacts
--- client/src/pages/Chat.jsx
+++ client/src/pages/Chat.jsx
@@ -1,44 +1,52 @@
 import axios from 'axios';
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import styled from "styled-components"
+import styled from 'styled-components';
 import { allUserRoute } from '../utils/APIRoutes';
 import Contacts from '../components/Contacts';
 
 const Chat = () => {
   const [contacts, setContacts] = useState();
   const [currentUser, setCurrentUser] = useState(undefined);
-  const navigate = useNavigate()
+  const navigate = useNavigate();
 
-  useEffect(async () => {
-    if(!localStorage.getItem('chat-app-user')) {
-      navigate("/login");
-    } else {
-        setCurrentUser(await JSON.parse(localStorage.getItem("chat-app-user")));
-    }
-  },[])
-
-  useEffect(async () =>{
-    if (currentUser) {
-      if (currentUser.isAvatarImageSet) {
-        const data = await axios.get(`${allUserRoute}/${currentUser._id}`);
-        setContacts(data.data);
+  useEffect(() => {
+    const fetchUser = async () => {
+      if (!localStorage.getItem('chat-app-user')) {
+        navigate('/login');
       } else {
-        navigate("/setAvatar");
+        setCurrentUser(await JSON.parse(localStorage.getItem('chat-app-user')));
       }
-    }
-  },[currentUser])
+    };
+
+    fetchUser();
+  }, [navigate]);
+
+  useEffect(() => {
+    const fetchContacts = async () => {
+      if (currentUser) {
+        if (currentUser.isAvatarImageSet) {
+          const { data } = await axios.get(`${allUserRoute}/${currentUser._id}`);
+          setContacts(data);
+        } else {
+          navigate('/setAvatar');
+        }
+      }
+    };
+
+    fetchContacts();
+  }, [currentUser, navigate]);
 
   return (
-   <Container>
-        <div className="container">
-            <Contacts contacts={contacts}/>
-        </div>
-   </Container>
-  )
-}
+    <Container>
+      <div className="container">
+        <Contacts contacts={contacts} currentUser={currentUser} />
+      </div>
+    </Container>
+  );
+};
 
-const Container = styled.div `
+const Container = styled.div`
   height: 100vh;
   width: 100vw;
   display: flex;
@@ -47,17 +55,17 @@ const Container = styled.div `
   gap: 1rem;
   align-items: center;
   background-color: #131324;
-  .container{
+  .container {
     height: 85vh;
     width: 85vw;
     background-color: #00000076;
-    display:grid;
+    display: grid;
     grid-template-columns: 25% 75%;
 
     @media screen and (min-width: 720px) and (max-width: 1080px) {
       grid-template-columns: 35% 65%;
     }
   }
-`
+`;
 
-export default Chat
+export default Chat;
